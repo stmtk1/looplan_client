c05451d84bc81c2cc3d6628e793670c28613631c
Motoki Saito <stmtk13044032@gmail.com>
2023-07-09 15:49:21 +0900

    use create success

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
-import React, { useCallback } from "react";
+'use client';
+import React, { useCallback, useState } from "react";
 
-export default async function Page() {
-  const ret = await (await fetch('http://localhost:3000')).json();
-  console.log(useCallback);
+type InputElement = { target: { value: React.SetStateAction<string>; }; };
+
+export default function Page() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   const onclick = useCallback(async () => {
-    const ret = await (await fetch('http://localhost:3000')).json();
+    const body = {
+      user_name: userName,
+      password,
+    }
+    const ret = await (await fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json'} },
+      )).json();
     console.log(ret);
   }, []);
+
+  const changeUserName = useCallback((e: InputElement) => { setUserName(e.target.value)}, [ setUserName ]);
+  const changePassword = useCallback((e: InputElement) => { setPassword(e.target.value)}, [ setPassword ]);
   return <div>
-    <h1>{ret['hello']}</h1>
-    <button onClick={onclick}>hello world</button>
+    <div>
+      <label>ユーザー名</label>
+      <input value={userName} type='input' onChange={changeUserName} />
+    </div>
+    <div>
+      <label>パスワード</label>
+      <input value={password} type='password' onChange={changePassword} />
+    </div>
+    <button onClick={onclick}>送信</button>
   </div>;
 }
